9034f61810fc5bfcebee83267cdc06a6ecd70247
Arcterus <Arcterus@mail.com>
2014-07-07 22:57:54 -0700

    Make build.xml portable

--- pid_struct.c
+++ pid_struct.c
@@ -8,7 +8,7 @@ typedef struct {
     double int_error;
     double control;
 } PID;
- 
+
 void pid_zeroize(PID* pid) {
     pid->prev_error = 0;
     pid->int_error = 0;
@@ -19,7 +19,7 @@ void pid_init(PID* pid, double K_p, double K_i, double K_d) {
     pid->integral_gain = K_i;
     pid->derivative_gain = K_d;
 }
- 
+
 //dt is the time difference
 //get curr_error from atan, distance away from object, etc. 
 void pid_update(PID* pid, double curr_error, double dt) {
@@ -28,10 +28,10 @@ void pid_update(PID* pid, double curr_error, double dt) {
     double p_term;
     double i_term;
     double d_term;
- 
+
     pid->int_error += (curr_error * dt);
     diff = ((curr_error - pid->prev_error) / dt);
- 
+
     // scaling
     p_term = (pid->proportional_gain * curr_error);
     i_term = (pid->integral_gain * pid->int_error);
